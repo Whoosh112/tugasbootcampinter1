c27e174823480c9fe754eefcb4fb9d1f7f6ae7e9
Ricky Irawan <rickyirawan9@gmail.com>
2025-02-14 11:57:04 +0700

    menyelesaikan layout dasar dari register dan login (styling component pending)

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,26 +1,39 @@
+import Logo from "../Components/LogoChill";
+import Username from "../Components/Username";
+import KataSandi from "../Components/KataSandi";
+import MasalahLogin from "../Components/MasalahLogin";
+import LoginButtonBox from "../Components/LoginButtonBox";
+import KataSandiUlang from "../Components/KataSandiUlang";
+import "./cssPages/styleRegister.css"
+
 
 
 function Register () {
     return (
-        <>
-             <div className="logomasuk">
-            </div>
-            <div className="welcomemessage">
-              <h1>Masuk</h1>
-              Selamat datang!
-            </div>
-              <div className="username">
-              </div>
-              <div className="password">
-              </div>
-              <div className="password">
-              </div>
-              <div className="masalahlogin">
-            <div>Sudah punya akun? <b>Masuk</b></div>
+  <div className="loginscreen">
+      <div className="logo">
+        <Logo />
+      </div>
+        <div className="judul">
+          <h1>Daftar</h1>
+          <p>Selamat datang!</p>
+        </div>
+          <div className="username">
+            <Username />
           </div>
-              <div className="loginbuttonbox">
-            </div>
-        </>
+          <div className="katasandi">
+           <KataSandi />
+          </div>
+          <div className="katasandiulang">
+           <KataSandiUlang />
+          </div>
+          <div className="masalahlogin">
+            <MasalahLogin />
+          </div>
+          <div className="loginbuttonbox">
+            <LoginButtonBox/>
+        </div>
+  </div>
         )
 };
 
